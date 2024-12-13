de57f562378481137949b6ef839fc6c5adb8121c
NozimbekUralov <nozimurolov1@gmail.com>
2024-12-13 11:23:31 +0500

    done

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Phone {
+ typedef struct {
     char name[64];
-    char brand[64];
-    double price;
-    int ram;
-    char color[32];
-};
+    char surname[64];
+    int age;
+    int grade;
+} Student;
 
 int main(){
 
-    struct Phone phone1 = {"S24", "Samsung", 1200.00, 12, "Red"};
+    Student student = {"Nozimbek", "Uralov", 12, 5};
 
-    printf("%s\n", phone1.name);  // member operator
-    printf("%s\n", phone1.brand);
-    printf("%.2lf\n", phone1.price);
-    printf("%d\n", phone1.ram);
-    printf("%s\n", phone1.color);
+    printf("%s\n", student.name);  // member operator
+    printf("%s\n", student.surname);
+    printf("%d\n", student.age);
+    printf("%d\n", student.grade);
 
     return 0;
 }
